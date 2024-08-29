ad95a7cd517b42294a10fa6df4d4c4ba9278e793
JackelineBritt0 <jackeline.brito.silva@escola.pr.gov.br>
2024-08-29 11:27:31 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+   background("pink");
+}
+function draw() {
+  stroke ("blue");
+  fill ("purple");
+  if (mouseIsPressed) { rect(mouseX, mouseY,20,20);
+                      }
+}
